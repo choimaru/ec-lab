4b6adec5a01ac832df103b2adb4439312078b195
choimaru <94576178+choimaru@users.noreply.github.com>
2024-04-22 16:23:11 +0900

    kebab-case以外も許容

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -5,4 +5,8 @@ module.exports = {
     'stylelint-config-standard-vue/scss', // vueファイルに対応
     'stylelint-config-recess-order', // プロパティ並び替え
   ],
+  rules: {
+    'selector-id-pattern': null, // idでkebab-case以外も許容
+    'selector-class-pattern': null, // classでkebab-case以外も許容
+  },
 };
