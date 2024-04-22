7adefb12687f256df06728f477a0c3eba3f5f6f7
choimaru <94576178+choimaru@users.noreply.github.com>
2024-04-22 15:17:22 +0900

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,12 @@ export default defineNuxtConfig({
   css: ['~/assets/styles/destyle.css', '~/assets/styles/style.scss'],
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL,
+    head: {
+      title: 'EC-Lab',
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/round_flask_black.svg' }],
+      htmlAttrs: {
+        lang: 'ja',
+      },
+    },
   },
 });
