359ab4a55cdae82215a3d9f8de31918088a76012
choimaru <94576178+choimaru@users.noreply.github.com>
2024-05-06 16:10:57 +0900

    共通タイトル

---
+++ src/components/H1Title.vue
@@ -0,0 +1,14 @@
+<
+<template>
+  <h1 class="title"><slot /></h1>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  padding-bottom: 4px;
+  margin-bottom: 16px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #5f6f81;
+}
+</style>
