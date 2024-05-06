5459adbb81c1894856c7b3773366e1dd6cabf092
choimaru <94576178+choimaru@users.noreply.github.com>
2024-05-06 16:10:27 +0900

    共通テキスト

---
+++ src/components/InputText.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+type Props = {
+  type?: 'text' | 'password' | 'date' | 'datetime-local' | 'number';
+  size?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  disabled?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), { type: 'text' });
+const model = defineModel();
+</script>
+
+<template>
+  <input
+    :type="props.type"
+    :class="[{ g_disabled: props.disabled }, props.size]"
+    v-model="model"
+    :disabled="props.disabled"
+  />
+</template>
+
+<style lang="scss" scoped>
+input[type='number'] {
+  padding-right: 2px;
+  text-align: right;
+}
+
+input[type='text'],
+input[type='date'],
+input[type='datetime-local'],
+input[type='number'],
+input[type='password'] {
+  height: 32px;
+  padding-left: 4px;
+  border: 1px solid #ced4da;
+
+  &:focus {
+    border: 1px solid #79bb32;
+    outline: none;
+  }
+}
+
+.xs {
+  width: 40px;
+}
+
+.s {
+  width: 90px;
+}
+
+.m {
+  width: 140px;
+}
+
+.l {
+  width: 190px;
+}
+
+.xl {
+  width: 240px;
+}
+</style>
