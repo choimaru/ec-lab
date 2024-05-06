ed4a071b641b976db255d3d1c7a6e631bfe2401b
choimaru <94576178+choimaru@users.noreply.github.com>
2024-05-06 16:13:13 +0900

    基底アイコン

---
+++ src/components/icon/IconSvgBase.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+export type BaseProps = {
+  width?: number;
+  height?: number;
+  color?: string;
+  disabled?: boolean;
+};
+
+const props = withDefaults(defineProps<BaseProps>(), {
+  width: 16,
+  height: 16,
+  color: '#fff',
+});
+
+const color = ref(props.disabled ? '#dcdcdc' : props.color);
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="props.width"
+    :height="props.height"
+    viewBox="0 0 512 512"
+    role="presentation"
+  >
+    <g :fill="color">
+      <slot />
+    </g>
+  </svg>
+</template>
